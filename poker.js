d96e4308f2810056b6dcac8758241513bfcf8a38
davelens <github@davelens.be>
2015-06-11 00:23:04 +0200

    add initial version of poker.{js,css}

--- poker.js
+++ poker.js
@@ -1,35 +1,28 @@
 /*!
- * jQuery Poker notification Plugin v1.0.0
- * https://github.com/davelens/jquery-poker
+ * poker.js notification plugin
+ * https://github.com/davelens/poker-js
  *
+ * Version 1.0.0
  * Copyright 2015 Dave Lens
  * Released under the MIT license
  */
 ;(function($){
-  var context = this;
-  var methods = {};
   var defaults = {
     close_button: true,
+    container: {
+      css: {
+        maxWidth: '400px',
+        padding: '0 12px',
+        position: 'fixed',
+        right: '0',
+        top: '12px',
+        zIndex: '9999'
+      }
+    },
     timeout: 5000
   };
 
-  function Poke(message, options) {
-    this._message = message;
-    this._options = options;
-  };
-
-  Poke.prototype.html = function() {
-    var box = $('<div class="poke" />');
-    this._options.close_button && box.append(this.close_button());
-    $('<p />').html(this._message).appendTo(box);
-    return box;
-  };
-
-  Poke.prototype.close_button = function() {
-    return $('<span>&times;</span>') ;
-  };
-
-  var Poker = window.Poker = { _container: null };
+  window.Poker = { _container: null };
 
   Poker._poke_click_listener = function(e) {
     $(this).fadeOut({
@@ -42,6 +35,22 @@
     return $('#poker-container').length > 0;
   };
 
+  Poker.Notification = function(message, options) {
+    this._message = message;
+    this._options = options;
+  };
+
+  Poker.Notification.prototype.html = function() {
+    var box = $('<div class="poke" />');
+    this._options.close_button && box.append(this.close_button());
+    $('<p />').html(this._message).appendTo(box);
+    return box;
+  };
+
+  Poker.Notification.prototype.close_button = function() {
+    return $('<span>&times;</span>') ;
+  };
+
   Poker.container = function() {
     this._container = this._container || $('<div id="poker-container" />');
     !this._exists() && this._container.appendTo('body');
@@ -49,10 +58,12 @@
   };
 
   Poker.poke = function(message, options) {
-    var options = $.extend(defaults, options);
-    var poke = new Poke(message, options);
-    var pokebox = poke.html().hide().prependTo(this.container()).fadeIn()
-    pokebox.on('click', this._poke_click_listener);
-    pokebox.delay(options.timeout).fadeOut(function(){ this.remove(); });
+    var options = $.extend({}, defaults, options);
+    var poke = new Poker.Notification(message, options);
+    var container = this.container().css(options.container.css);
+    var notification = poke.html().hide().prependTo(container).fadeIn();
+    notification
+      .on('click', this._poke_click_listener)
+      .delay(options.timeout).fadeOut(function(){ this.remove(); });
   };
 })(jQuery);
